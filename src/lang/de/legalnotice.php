54ae791f705b27a116a0cf46211099007fb2310b
Anonymous <noreply@weblate.org>
2025-06-20 14:11:39 +0200

    Translated using Weblate (German)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: Eventula Manager/Eventula Manager legalnotice component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-legalnotice-component/de/

--- src/lang/de/legalnotice.php
+++ src/lang/de/legalnotice.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Authentication Language Lines
@@ -12,8 +10,6 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-
     'legalnoticetitle' => 'Impressum',
     'privacy' => 'Datenschutzerklärung',
-    
 ];
