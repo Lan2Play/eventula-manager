21041ed41e92ecf7bc7e1f48554ad520a9aae2e1
Markus Kohn <post@markus-kohn.com>
2025-10-05 14:03:29 +0200

    fix: update language file comments to correctly reflect date key usage in en and de locales

--- src/lang/de/date.php
+++ src/lang/de/date.php
@@ -4,12 +4,9 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
+    | Dates
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
     |
     */
     'time_delimiter' => 'um',
